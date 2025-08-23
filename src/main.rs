35566e69dd158a4b406a806ccec570be85eb4954
Kshitij Lauria <kslauria@gmail.com>
2025-08-23 11:12:59 -0400

    rotation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod linalg;
 mod tga;
 mod wavefront_obj;
 
+use std::f32;
+
 use crate::image::*;
 use crate::linalg::*;
 
@@ -46,7 +48,6 @@ fn linef32(ax: f32, ay: f32, bx: f32, by: f32, image: &mut Image, color: Color) 
 
 fn main() -> std::io::Result<()> {
     let s = 800u16;
-    let mut image = Image::new(s, s);
 
     let model = wavefront_obj::Model::from_file("assets/diablo3_pose.obj").unwrap();
     println!(
@@ -55,24 +56,36 @@ fn main() -> std::io::Result<()> {
         model.faces.len()
     );
 
-    let s = image.width() as f32;
     let one = vec3(1.0, 1.0, 1.0);
-    for face in model.faces.iter() {
-        let a = (one + model.vertices[face[0]]) * s / 2.0001;
-        let b = (one + model.vertices[face[1]]) * s / 2.0001;
-        let c = (one + model.vertices[face[2]]) * s / 2.0001;
 
-        linef32(a.x, a.y, b.x, b.y, &mut image, RED);
-        linef32(b.x, b.y, c.x, c.y, &mut image, RED);
-        linef32(c.x, c.y, a.x, a.y, &mut image, RED);
-    }
+    for (idx, angle) in (0..360).step_by(20).enumerate() {
+        let mut image = Image::new(s, s);
+        let s = image.width() as f32;
 
-    for vertex in model.vertices.iter() {
-        let v = (one + *vertex) / 2.0001;
-        image.set((v.x * s) as usize, (v.y * s) as usize, WHITE);
+        let angle = angle as f32 * 2.0 * f32::consts::PI / 360.0;
+        let cos_angle = angle.cos();
+        let sin_angle = angle.sin();
+
+        for face in model.faces.iter() {
+            let mut a = model.vertices[face[0]];
+            let mut b = model.vertices[face[1]];
+            let mut c = model.vertices[face[2]];
+
+            a.x = a.x * cos_angle + a.z * sin_angle;
+            b.x = b.x * cos_angle + b.z * sin_angle;
+            c.x = c.x * cos_angle + c.z * sin_angle;
+
+            let a = (one + a) * s / 2.0001;
+            let b = (one + b) * s / 2.0001;
+            let c = (one + c) * s / 2.0001;
+
+            linef32(a.x, a.y, b.x, b.y, &mut image, RED);
+            linef32(b.x, b.y, c.x, c.y, &mut image, RED);
+            linef32(c.x, c.y, a.x, a.y, &mut image, RED);
+        }
+        let tga = tga::TgaFile::from_image(image);
+        tga.save_to_path(format!("output-{:02}.tga", idx).as_str())?;
     }
-    let tga = tga::TgaFile::from_image(image);
-    tga.save_to_path("output.tga")?;
 
     Ok(())
 }
